02c902809fcf5e43e83966eeeba6cdbcfe44ce31
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-14 17:50:11 +0900

    chore: 롬복 어노테이션 의존성 추가

---
+++ src/main/java/com/example/chatgateway/filter/AuthFilter.java
@@ -0,0 +1,17 @@
+package com.example.chatgateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class AuthFilter implements GatewayFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+
+        return null;
+    }
+}
